c2efb1dab6114bc39ae5d790f45794d260c8a924
sergey-komar <kursev81@yandex.ru>
2023-10-01 17:48:43 +0300

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,8 @@
+$(function () {
+    $('.slider-home__inner').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow: '<button type="button" class="slick-prev-home"></button>',
+        nextArrow: '<button type="button" class="slick-next-home"></button>'
+    })
+})
